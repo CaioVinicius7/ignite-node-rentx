c427819320018b97bfa9d12c3be15d9796aebec3
Caio Vinícius <caio1525pereira@gmail.com>
2021-12-16 11:30:23 -0300

    Inserindo tipagem para categoria

--- src/routes/categories.routes.ts
+++ src/routes/categories.routes.ts
@@ -1,18 +1,24 @@
 import { Router } from "express";
-import { v4 as uuidv4 } from "uuid";
+
+import { Category } from "../models/category";
 
 const categoriesRoutes = Router();
 
-const categories = [];
+// Define que categories será um array de Category
+const categories: Category[] = [];
 
 categoriesRoutes.post("/", (req, res) => {
   const { name, description } = req.body;
 
-  const category = {
-    id: uuidv4(),
+  // Chama o construtor para definir o id
+  const category: Category = new Category();
+
+  // Adiciona as atributos ao objeto
+  Object.assign(category, {
     name,
-    description
-  };
+    description,
+    created_at: new Date()
+  });
 
   categories.push(category);
 
