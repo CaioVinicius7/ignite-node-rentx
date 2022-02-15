453815cd3cb4e0a78b97fc71ec2a439e6f749428
Caio Vinícius <caio1525pereira@gmail.com>
2022-02-15 10:29:21 -0300

    Criando upload de arquivos

--- src/routes/categories.routes.ts
+++ src/routes/categories.routes.ts
@@ -1,9 +1,14 @@
 import { Router } from "express";
+import multer from "multer";
 
 import { createCategoryController } from "../modules/cars/useCases/createCategory";
 import { listCategoriesController } from "../modules/cars/useCases/listCategories";
 
 const categoriesRoutes = Router();
+
+const upload = multer({
+	dest: "./tmp"
+});
 
 // Cria uma nova categoria
 categoriesRoutes.post("/", (req, res) => {
@@ -13,6 +18,13 @@ categoriesRoutes.post("/", (req, res) => {
 // Lista as categorias
 categoriesRoutes.get("/", (req, res) => {
 	return listCategoriesController.handle(req, res);
+});
+
+//
+categoriesRoutes.post("/import", upload.single("file"), (req, res) => {
+	const { file } = req;
+	console.log(file);
+	return res.send();
 });
 
 export { categoriesRoutes };
