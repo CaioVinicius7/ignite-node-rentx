df886b175ac5c5440b8a72759b16f6db689e55b9
Caio Vinícius <caio1525pereira@gmail.com>
2022-04-13 11:52:12 -0300

    Caso de uso do cadastro de imagens do carro

--- src/modules/cars/useCases/uploadCarImage/UploadCarImageController.ts
+++ src/modules/cars/useCases/uploadCarImage/UploadCarImageController.ts
@@ -1,5 +1,43 @@
-class UploadCarImageController {
-	handle() {}
+import { Request, Response } from "express";
+import { container } from "tsyringe";
+
+import { UploadCarImagesUseCase } from "./UploadCarImageUseCase";
+
+interface IFiles {
+	filename: string;
 }
 
-export { UploadCarImageController };
+class UploadCarImagesController {
+	async handle(req: Request, res: Response): Promise<Response> {
+		const { id } = req.params;
+		const image = req.files;
+		console.log(
+			"🚀 ~ file: UploadCarImageController.ts ~ line 14 ~ UploadCarImagesController ~ handle ~ image",
+			image
+		);
+		const images = req.files as IFiles[];
+		console.log(
+			"🚀 ~ file: UploadCarImageController.ts ~ line 14 ~ UploadCarImagesController ~ handle ~ images",
+			images
+		);
+
+		const uploadCarImagesUseCase = container.resolve(UploadCarImagesUseCase);
+
+		const images_name = images.map((file) => {
+			return file.filename;
+		});
+		console.log(
+			"🚀 ~ file: UploadCarImageController.ts ~ line 29 ~ UploadCarImagesController ~ constimages_name=images.map ~ images_name",
+			images_name
+		);
+
+		await uploadCarImagesUseCase.execute({
+			car_id: id,
+			images_name
+		});
+
+		return res.status(201).send();
+	}
+}
+
+export { UploadCarImagesController };
