4203cf705f039c8c587dad7ec371b209886da1b4
Caio Vinícius <caio1525pereira@gmail.com>
2022-04-14 16:44:14 -0300

    Trabalhando com datas com dayjs

--- src/modules/rentals/useCases/createRental/CreateRentalUseCase.ts
+++ src/modules/rentals/useCases/createRental/CreateRentalUseCase.ts
@@ -1,7 +1,12 @@
+import dayjs from "dayjs";
+import utc from "dayjs/plugin/utc";
+
 import { ICreateRentalDTO } from "@modules/rentals/dtos/ICreateRentalDTO";
 import { Rental } from "@modules/rentals/infra/typeorm/entities/rental";
 import { IRentalsRepository } from "@modules/rentals/repositories/IRentalsRepository";
 import { AppError } from "@shared/errors/AppErro";
+
+dayjs.extend(utc);
 
 class CreateRentalUseCase {
 	constructor(private rentalsRepository: IRentalsRepository) {}
@@ -11,6 +16,8 @@ class CreateRentalUseCase {
 		car_id,
 		expected_return_date
 	}: ICreateRentalDTO): Promise<Rental> {
+		const minimumHours = 24;
+
 		const carUnavailable = await this.rentalsRepository.findOpenRentalByCar(
 			car_id
 		);
@@ -25,6 +32,19 @@ class CreateRentalUseCase {
 
 		if (rentalOpenToUser) {
 			throw new AppError("There's a rental in progress for user!");
+		}
+
+		const expectedReturnDateFormat = dayjs(expected_return_date)
+			.utc()
+			.local()
+			.format();
+
+		const dateNow = dayjs().utc().local().format();
+
+		const compare = dayjs(expectedReturnDateFormat).diff(dateNow, "hours");
+
+		if (compare < minimumHours) {
+			throw new AppError("Invalid return time!");
 		}
 
 		const rental = await this.rentalsRepository.create({
